410c65c072ac17d00a5ffebfb93554c75bac4d55
Sasha Koshka <sashakoshka@tebibyte.media>
2023-01-16 00:36:23 -0500

    Label conforms to new API

--- elements/basic/label.go
+++ elements/basic/label.go
@@ -28,17 +28,13 @@ func NewLabel (text string, wrap bool) (element *Label) {
 	return
 }
 
-// Handle handles and event.
-func (element *Label) Handle (event tomo.Event) {
+func (element *Label) Resize (width, height int) {
 	switch event.(type) {
 	case tomo.EventResize:
-		resizeEvent := event.(tomo.EventResize)
-		element.core.AllocateCanvas (
-			resizeEvent.Width,
-			resizeEvent.Height)
+		element.core.AllocateCanvas(width, height)
 		if element.wrap {
-			element.drawer.SetMaxWidth (resizeEvent.Width)
-			element.drawer.SetMaxHeight(resizeEvent.Height)
+			element.drawer.SetMaxWidth(width)
+			element.drawer.SetMaxHeight(height)
 		}
 		element.draw()
 	}
