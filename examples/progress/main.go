1fee6ab9e6031da9946e294848a7001e46f3b143
Sasha Koshka <sashakoshka@tebibyte.media>
2023-01-17 20:30:58 -0500

    Increased the silly

--- examples/progress/main.go
+++ examples/progress/main.go
@@ -2,6 +2,7 @@ package main
 
 import "time"
 import "git.tebibyte.media/sashakoshka/tomo"
+import "git.tebibyte.media/sashakoshka/tomo/popups"
 import "git.tebibyte.media/sashakoshka/tomo/layouts"
 import "git.tebibyte.media/sashakoshka/tomo/elements/basic"
 import _ "git.tebibyte.media/sashakoshka/tomo/backends/x"
@@ -30,11 +31,16 @@ func run () {
 }
 
 func fill (bar *basic.ProgressBar) {
-	println("-")
 	for progress := 0.0; progress < 1.0; progress += 0.01 {
 		time.Sleep(time.Second / 24)
-		tomo.Do(func () {
+		tomo.Do (func () {
 			bar.SetProgress(progress)
 		})
 	}
+	tomo.Do (func () {
+		popups.NewDialog (
+			popups.DialogKindInfo,
+			"I am here",
+			"Don't look outside your window.")
+	})
 }
